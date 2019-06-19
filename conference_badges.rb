5ed6de0b866222bf18893f0a335ef8075aa83c66
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 01:46:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,28 @@
-def badge_maker(array)
-  array.each do |name| 
-    return "Hello, my name is #{name}."
+peeps = ["Jim", "John", "Joe", "Peter", "Paul"]
+
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(peeps)
+  peeps.map do |name|
+  "Hello, my name is #{name}."
   end
+end
+
+def assign_rooms(peeps)
+  peeps.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(peeps)
+  batch_badge_creator(peeps).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(peeps).each do |assignment|
+    puts assignment
+  end
+end
+  
