faf28f56dc1dc11d400601ff420924ebed36d6ed
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 01:46:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,18 @@ def batch_badge_creator(peeps)
 end
 
 def assign_rooms(peeps)
+  peeps.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(peeps)
+  batch_badge_creator(peeps).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(peeps).each do |assignment|
+    puts assignment
+  end
+end
   
