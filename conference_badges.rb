483922fbf1a310acaec6d6a4b05197c7401f8a0d
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 01:41:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
-def batch_badge_create(array)
+def batch_badge_creator(array)
   array.each do |name|
   return "Hello, my name is #{name}."
 end
